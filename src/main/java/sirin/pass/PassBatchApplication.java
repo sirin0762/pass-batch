c1aebb3399eaf138ac969d31e4012c2a9f4f7aaa
신용진 <tlsdydwls123@gmail.com>
2023-05-01 22:53:17 +0900

    FEAT : batch, jpa config 설정

--- src/main/java/sirin/pass/PassBatchApplication.java
+++ src/main/java/sirin/pass/PassBatchApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@EnableBatchProcessing
 @SpringBootApplication
 public class PassBatchApplication {
 

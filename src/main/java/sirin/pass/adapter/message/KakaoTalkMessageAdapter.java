77aeaf05ab9ab161a50af2c5df5276adefc3b9d7
sirin0762 <tlsdydwls123@gmail.com>
2023-05-11 21:44:24 +0900

    CHORE : Webflux 의존성 제거 및 Web Mvc 의존성 추가

--- src/main/java/sirin/pass/adapter/message/KakaoTalkMessageAdapter.java
+++ src/main/java/sirin/pass/adapter/message/KakaoTalkMessageAdapter.java
@@ -1,38 +1,36 @@
 package sirin.pass.adapter.message;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
 import sirin.pass.config.KakaoTalkMessageConfig;
 
 @Service
 public class KakaoTalkMessageAdapter {
 
-    private final WebClient webClient;
+//    private final WebClient webClient;
 
     public KakaoTalkMessageAdapter(KakaoTalkMessageConfig config) {
-        webClient = WebClient.builder()
-            .baseUrl(config.getHost())
-            .defaultHeaders(h -> {
-                h.setBearerAuth(config.getToken());
-                h.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-            })
-            .build();
+//        webClient = WebClient.builder()
+//            .baseUrl(config.getHost())
+//            .defaultHeaders(h -> {
+//                h.setBearerAuth(config.getToken());
+//                h.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+//            })
+//            .build();
     }
 
     public boolean sendKakaoTalkMessage(final String uuid, final String text) {
-        KakaoTalkMessageResponse response = webClient.post().uri("/v2/api/talk/memo/default/send")
-            .body(BodyInserters.fromValue(new KakaoTalkMessageRequest(uuid, text)))
-            .retrieve()
-            .bodyToMono(KakaoTalkMessageResponse.class)
-            .block();
+//        KakaoTalkMessageResponse response = webClient.post().uri("/v2/api/talk/memo/default/send")
+//            .body(BodyInserters.fromValue(new KakaoTalkMessageRequest(uuid, text)))
+//            .retrieve()
+//            .bodyToMono(KakaoTalkMessageResponse.class)
+//            .block();
+//
+//        if (response == null || response.getSuccessfulReceiverUuids() == null) {
+//            return false;
+//        }
 
-        if (response == null || response.getSuccessfulReceiverUuids() == null) {
-            return false;
-        }
-
-        return response.getSuccessfulReceiverUuids().size() > 0;
+//        return response.getSuccessfulReceiverUuids().size() > 0;
+        return false;
     }
 
 }
